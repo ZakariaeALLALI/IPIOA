f56bc366225190eca894338e75523573fdde5c09
Zakariae Allali <zakariae.allali@e-polytechnique.ma>
2024-12-17 17:35:13 +0100

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,16 @@
 <?php
+$servername = "sql206.infinityfree.com";  
+$username = "if0_37936234";  
+$password = "DcdhJQDrV5 ";  
+$dbname = "if0_37936234_ecom";  
 
-$conn = mysqli_connect('localhost','root','','ecom');
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if(!$conn){
-    echo "connection unsuccessful";
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
+echo "Connected successfully";
 ?>
